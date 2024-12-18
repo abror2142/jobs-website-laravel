f4475e049f261a8243c3f5d5d17377a412de4689
Abrorbek Ismoilov <abror2142@gmail.com>
2024-12-18 19:09:42 +0500

    Job model, factory, migration and seeder implementation

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->jobTitle(),
+            'short_description' => $this->faker->sentence(20),
+            'full_description' => $this->faker->paragraph(5)
+        ];
+    }
+}
