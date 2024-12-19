35b4f742852e95773fdc1474edb20699fcb50755
Abrorbek Ismoilov <abror2142@gmail.com>
2024-12-19 13:18:38 +0500

    Adding Tag model, factory and migration in many-to-many relation with Job model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Tag;
 
 class Job extends Model
 {
@@ -12,8 +13,13 @@ class Job extends Model
     use HasFactory;
 
     protected $table = 'job_openings';
+    protected $guarded = [];
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function tags ( ) {
+        return $this->belongsToMany(Tag::class);
     }
 }
